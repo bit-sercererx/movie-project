feb0bee696737ce5b6468e75241f4559f497af8a
mohamedali0122 <mohamedalhelo909@gmail.com>
2024-04-16 16:13:22 +0300

    creat moves page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./Componets/Pages/Navbar";
 import Footer from "./Componets/Pages/Footer";
+import MovesPages from "./Componets/Pages/Moviepages";
 function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
       {/* <Footer /> */}
+      <MovesPages />
     </div>
   );
 }
---
+++ src/Componets/Pages/Moviepages.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function MovesPages() {
+  return (
+    <div className='w-full h-auto flex flex-col bg-gray-700'>
+       <div className='flex items-center justify-start h-14  pb-14'><p className='pt-8 pl-6 md:pl-10 lg:pl-16 xl:pl-14 text-xl text-white'>moves page</p></div>
+       <div className=' pb-6 mx-auto flex items-center justify-center flex-wrap gap-4 lg:gap-6 w-[95%] lg:w-[95%] h-auto'>
+          <div className='w-28 h-40 md:w-40 md:h-52 bg-fuchsia-100 rounded-lg'>
+              d
+          </div>
+          <div className='w-28 h-40 md:w-40 md:h-52 bg-fuchsia-100 rounded-lg'>
+              d
+          </div>
+          <div className='w-28 h-40 md:w-40 md:h-52 bg-fuchsia-100 rounded-lg'>
+              d
+          </div>
+          <div className='w-28 h-40 md:w-40 md:h-52 bg-fuchsia-100 rounded-lg'>
+              d
+          </div>
+          <div className='w-28 h-40 md:w-40 md:h-52 bg-fuchsia-100 rounded-lg'>
+              d
+          </div>
+          <div className='w-28 h-40 md:w-40 md:h-52 bg-fuchsia-100 rounded-lg'>
+              d
+          </div>
+          <div className='w-28 h-40 md:w-40 md:h-52 bg-fuchsia-100 rounded-lg'>
+              d
+          </div>
+          <div className='w-28 h-40 md:w-40 md:h-52 bg-fuchsia-100 rounded-lg'>
+              d
+          </div>
+          
+          
+       </div>
+       <div className=' w-full h-14 flex items-center justify-center'>
+         <button className='bg-red-700 px-4 py-1 rounded-xl text-white font-bold '>See more</button>
+       </div>
+    </div>
+  )
+}
+
+export default MovesPages
