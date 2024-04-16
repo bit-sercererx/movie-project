55c18eed7b509486474b7a326ff43a892f3d9f26
mohamedali0122 <mohamedalhelo909@gmail.com>
2024-04-16 19:04:05 +0300

    create genres

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./Componets/Pages/Navbar";
 import Footer from "./Componets/Pages/Footer";
 import MovesPages from "./Componets/Pages/Moviepages";
+import Genres from "./Componets/Pages/Genres";
 function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
       {/* <Footer /> */}
       {/* <MovesPages /> */}
+      <Genres />
     </div>
   );
 }
