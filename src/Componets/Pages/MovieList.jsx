54f559797a76ab100c78ec31113ca30dd81a019a
bit-sercererx <ameerahmed202397@gmail.com>
2024-04-17 12:02:35 +0300

    done

--- src/Componets/Pages/MovieList.jsx
+++ src/Componets/Pages/MovieList.jsx
@@ -5,9 +5,10 @@ import { MovieItem } from './MovieItem'
 export const MovieList = ({movies}) => {
   return (
     <div>
-         <div className="mt-5 flex overflow-x-auto mx-6 " id='popular'>
+         <div className="mt-5 flex overflow-x-auto mx-6  " id='popular'>
                     
-        {movies.map(movie => <MovieItem  key={movie.id} movie={movie} />)}
+        {movies.map(movie => <MovieItem  key={movie.id} movie={movie} /> )}
+
             </div>
     </div>
   )
